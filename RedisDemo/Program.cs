531752e4b736e9714ca6c876bcf6fbe704c4ba7e
Huachao <534550354@qq.com>
2016-09-06 11:07:51 +0800

    第一次提交

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedisDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+          
+        }
+    }
+}
---
+++ RedisDemo/RedisHelp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    public static class RedisHelp
+    {
+        public static string ClentIp { get; set; }
+        public static int Prot { get; set; }
+        public static string Password { get; set; }
+        public static string ConnectionStr
+        {
+            get
+            {
+                return ClentIp + ":" + Prot;
+            }
+        }
+    }
+}
