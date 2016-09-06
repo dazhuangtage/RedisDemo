1a3542fc13f7c150478a0a77ad0eebeab00a26db
Huachao <534550354@qq.com>
2016-09-06 16:50:48 +0800

    增加了Redis帮助类

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Reflection;
 namespace RedisDemo
 {
     static class Program
@@ -11,9 +11,23 @@ namespace RedisDemo
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-          
+            Console.WriteLine("当前RedisIp:127.0.0.1");
+            Console.WriteLine("当前RedisPort:6379");
+            Console.WriteLine("当前RedisPwd:freep456187");
+            var redis = new RedisHelp("127.0.0.1", 6379,"freep456187");
+            using (var client = redis.Connection())
+            {
+                if (client.HashContainsEntry("url", "baidu.com"))
+                    Console.WriteLine("连接存在");
+                else
+                    Console.WriteLine("连接不存在");
+                Console.WriteLine("Url的HASH总数:"+ client.GetHashCount("url"));
+                
+                //client.Save();//持久化到文件
+                Console.WriteLine("打开连接成功......");
+            }
         }
     }
 }
--- RedisDemo/RedisHelp.cs
+++ RedisDemo/RedisHelp.cs
@@ -1,22 +1,35 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace RedisDemo
 {
-    public static class RedisHelp
+    public class RedisHelp
     {
-        public static string ClentIp { get; set; }
-        public static int Prot { get; set; }
-        public static string Password { get; set; }
-        public static string ConnectionStr
+        public string Host { get; set; }
+        public int Prot { get; set; }
+        public string Password { get; set; }
+        public string ConnectionStr
         {
             get
             {
-                return ClentIp + ":" + Prot;
+                return Host + ":" + Prot;
             }
+        }
+
+        public RedisHelp(string host, int prot, string password = "")
+        {
+            this.Host = host;
+            this.Prot = prot;
+            this.Password = password;
+        }
+
+        public RedisClient Connection()
+        {
+            RedisClient Client = new RedisClient(new RedisEndpoint() { Host = Host, Port = this.Prot, Password = this.Password });
+            return Client;
         }
     }
 }
